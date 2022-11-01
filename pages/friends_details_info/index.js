c3d1b474997320d112e2ba48469d8e6a3dcf4b3b
hany <hany93cuba@gmail.com>
2022-11-01 03:05:04 -0400

    add css styles for page 'friends details info'

--- pages/friends_details_info/index.js
+++ pages/friends_details_info/index.js
@@ -3,13 +3,11 @@ import React from "react";
 const FriendsList = () => {
     return (
         <section className="friends_details_info">
-            <div className="friends_details_info__centercontainer">
-                <div className="friends_details_info__back">
-                    <p>g</p>
-                </div>
-                <div className="friends_details_info__content">
-                    ghgfhg
-                </div>
+            <div className="friends_details_info__back">
+                <p>g</p>
+            </div>
+            <div className="friends_details_info__content">
+                ghgfhg
             </div>
         </section>
     );
