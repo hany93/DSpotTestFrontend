350600020ed5c1914db7cfb13d118b27771191be
hany <hany93cuba@gmail.com>
2022-11-03 17:25:40 -0400

    add information and css style to the friend details page card

--- pages/friends_details_info/index.js
+++ pages/friends_details_info/index.js
@@ -9,7 +9,7 @@ const FriendsList = () => {
                 </div>
             </div>
             <div className="friends_details_info__centercontainer">
-                <div>
+                <div className="friends_details_info__infoavatar">
                     <div>
                         <img src="images/avatar__150.png" className="friends_details_info__image" />
                         <span className="friends_details_info__ellipse" />
@@ -21,11 +21,39 @@ const FriendsList = () => {
                         </div>
                     </div>
                 </div>
-                <ul>
-                    <li id="info"><a href='#'>Info</a></li>
-                    <li id="photos"><a href='#'>Photos</a></li>
-                </ul>
-                <div className="friends_details_info__card">
+                <div className="friends_details_info__tab">
+                    <ul>
+                        <li id="info"><a href='#'>Info</a></li>
+                        <li id="photos"><a href='#'>Photos</a></li>
+                    </ul>
+                    <div className="friends_details_info__card">
+                        <div>
+                            <span className="friends_details_info__card__label">Bio:</span>
+                            <span className="friends_details_info__card__text">I'm very choosy. I'm also very suspicious, very irrational and I have a very short temper. I'm also extremely jealous and slow to forgive. Just so you know.</span>
+                        </div>
+                        <span className="friends_details_info__card__line" />
+                        <div>
+                            <span className="friends_details_info__card__label">Phone:</span>
+                            <span className="friends_details_info__card__text">(820) 289-1818</span>
+                        </div>
+                        <span className="friends_details_info__card__line" />
+                        <div>
+                            <span className="friends_details_info__card__label">Address:</span>
+                            <span className="friends_details_info__card__text">5190 Center Court Drive</span>
+                        </div>
+                        <div>
+                            <span className="friends_details_info__card__label">City:</span>
+                            <span className="friends_details_info__card__text">Spring</span>
+                        </div>
+                        <div>
+                            <span className="friends_details_info__card__label">State:</span>
+                            <span className="friends_details_info__card__text">TX</span>
+                        </div>
+                        <div>
+                            <span className="friends_details_info__card__label">Zipcode:</span>
+                            <span className="friends_details_info__card__text">77370</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
