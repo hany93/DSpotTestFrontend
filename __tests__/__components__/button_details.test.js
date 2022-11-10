c19deb813f0eaffb6f66315c9c177692944cae88
hany <hany93cuba@gmail.com>
2022-11-10 03:26:00 -0500

    Test button_details.test

--- __tests__/__components__/button_details.test.js
+++ __tests__/__components__/button_details.test.js
@@ -1,9 +1,28 @@
+import '@testing-library/jest-dom'
 import ButtonDetails from '@components/button/button_details'
 import { render, screen } from '@testing-library/react'
 
+const profile_id = { id: 1 }
+
 describe('ButtonDetails Component', () => {
-	it('Render button details', () => {
-		render(<ButtonDetails />)
-		expect(screen.getByRole('link').getAttribute('href')).toBe('/')
+	beforeEach(() => {
+		render(<ButtonDetails profile={profile_id} />)
+	})
+
+	it('Test role link', () => {
+		expect(screen.getByRole('link').getAttribute('href')).toBe(
+			'/profiles_details/' + profile_id.id
+		)
+	})
+
+	it('Test className profileslist__button', () => {
+		expect(
+			document.querySelector('div .profileslist__button')
+		).toBeInTheDocument()
+	})
+
+	it('Test Snapshot', () => {
+		var { container } = render(<ButtonDetails profile={profile_id} />)
+		expect(container).toMatchSnapshot()
 	})
 })
