65a9548b5eb4bde7ffe45c04977289f0e619204d
hany <hany93cuba@gmail.com>
2022-11-10 09:54:03 -0500

    Test element_grouping.test

---
+++ __tests__/__components__/element_grouping.test.js
@@ -0,0 +1,27 @@
+import '@testing-library/jest-dom'
+import ElementsGrouping from '@components/element_grouping'
+import { render, screen } from '@testing-library/react'
+
+const openModal = jest.fn()
+
+describe('Render CardInfo Component', () => {
+	beforeEach(() => {
+		render(
+			<ElementsGrouping
+				class_elementsgrouping='profiles_details__elementsgrouping'
+				name={'Profile'}
+				description={'Profile online'}
+			/>
+		)
+	})
+
+	it('Test text Profile online', () => {
+		expect(screen.getByText('Profile online')).toBeInTheDocument()
+	})
+
+	it('Test className profiles_details__elementsgrouping', () => {
+		expect(
+			document.querySelector('div .profiles_details__elementsgrouping')
+		).toBeInTheDocument()
+	})
+})
